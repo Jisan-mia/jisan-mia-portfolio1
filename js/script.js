93777bb0f8751a82081a57af5c221feea0252df9
Jisan-Mia <jisanmia47@gmail.com>
2020-05-10 20:22:04 -0700

    my personal website

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// Portfolio item filter
+const filterContainer = document.querySelector(".portfolio-filter"),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length,
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    totalPortfolioItem = portfolioItems.length;
+
+    for(let i = 0; i< totalFilterBtn;i++){
+        const filteringBtns=filterBtns[i];
+        filteringBtns.addEventListener("click", function(){
+            filterContainer.querySelector(".active").classList.remove("active");
+            this.classList.add("active");
+
+            const filterValue = this.getAttribute("data-filter");
+            for(let k = 0; k<totalPortfolioItem; k++){
+                if(filterValue === portfolioItems[k].getAttribute("data-category")){
+                    portfolioItems[k].classList.remove("hide");
+                    portfolioItems[k].classList.add("show");
+                }
+                else{
+                    portfolioItems[k].classList.remove("show");
+                    portfolioItems[k].classList.add("hide");
+                }
+                if(filterValue === "all"){
+                    portfolioItems[k].classList.remove("hide");
+                    portfolioItems[k].classList.add("show");
+                }
+            }
+        })
+    }
+
+//Portfolio Lightbox
+const lightbox = document.querySelector(".lightbox"),
+    lightboxImg = lightbox.querySelector(".lightbox-img"),
+    lightboxClose = lightbox.querySelector(".lightbox-close"),
+    lightboxText = lightbox.querySelector(".caption-text"),
+    lightboxCounter = lightbox.querySelector(".caption-counter");
+
+
+let itemIndex = 0;
+for(let i =0; i< totalPortfolioItem; i++){
+    portfolioItems[i].addEventListener("click", function(){
+        itemIndex = i;
+        changeItem();
+        toggleLightbox();
+    })
+}    
+
+//nextitem icon click
+function nextItem(){
+    if(itemIndex === totalPortfolioItem-1){
+        itemIndex = 0;
+    }
+    else{
+        itemIndex++;
+    }
+    changeItem()
+}
+function toggleLightbox(){
+    lightbox.classList.toggle("open");
+}
+//previous item icon click
+function prevItem(){
+    if(itemIndex === 0){
+        itemIndex= totalPortfolioItem-1
+    }
+    else{
+        itemIndex--;
+    }
+    changeItem()
+}
+
+function toggleLightbox(){
+    lightbox.classList.toggle("open");
+}
+//change portfolio item
+function changeItem(){
+    imgSrc = portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+    console.log(imgSrc)
+    lightboxImg.src = imgSrc;
+    lightboxText.innerHTML = portfolioItems[itemIndex].querySelector("h4").innerHTML;
+    lightboxCounter.innerHTML =(itemIndex+1) + " of " + totalPortfolioItem;
+}
+    
+//close lightbox
+lightbox.addEventListener("click", function(event){
+    if(event.target === lightboxClose || event.target === lightbox){
+        toggleLightbox()
+    }
+})
---
+++ js/styleSwitcher.js
@@ -0,0 +1,31 @@
+
+
+const links = document.querySelectorAll(".alternative-style");
+
+function setActiveStyle(color){
+    for (let i = 0; i < links.length; i++) {
+        if(color === links[i].getAttribute("title")){
+            links[i].removeAttribute("disabled");
+        }
+        else{
+            links[i].setAttribute("disabled", "true");
+        }   
+    }
+}
+
+//body skin
+const bodySkin = document.querySelectorAll(".body-skin");
+for (let i = 0; i < bodySkin.length; i++) {
+    bodySkin[i].addEventListener("change", function() {
+        if(this.value === 'dark'){
+            document.body.className = "dark";
+        }
+        else{
+            document.body.className = ""
+        }
+    })
+}
+
+document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
